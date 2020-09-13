02dd89c88671abd28de87bf04f8eecf78a3aec3d
s2sprint <69420623+s2sprint@users.noreply.github.com>
2020-09-13 22:34:59 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,67 @@
+basic.forever(function () {
+    if (338 < input.compassHeading() || input.compassHeading() < 22) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (23 < input.compassHeading() && input.compassHeading() < 67) {
+        basic.showLeds(`
+            # # # # .
+            # # . . .
+            # . # . .
+            # . . # .
+            . . . . #
+            `)
+    } else if (68 < input.compassHeading() && input.compassHeading() < 112) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (113 < input.compassHeading() && input.compassHeading() < 157) {
+        basic.showLeds(`
+            . . . . #
+            # . . # .
+            # . # . .
+            # # . . .
+            # # # # .
+            `)
+    } else if (158 < input.compassHeading() && input.compassHeading() < 202) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (203 < input.compassHeading() && input.compassHeading() < 247) {
+        basic.showLeds(`
+            # . . . .
+            . # . . #
+            . . # . #
+            . . . # #
+            . # # # #
+            `)
+    } else if (248 < input.compassHeading() && input.compassHeading() < 292) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . # # # #
+            . . . # #
+            . . # . #
+            . # . . #
+            # . . . .
+            `)
+    }
+})
